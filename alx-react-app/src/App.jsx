285153ba70265eeb4082dcd413cecba0aac6e855
Dinewo <dididlalisa@gmail.com>
2024-08-11 02:02:51 +0200

    Update App.jsx and Userprofile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import UserProfile from './components/UserProfile';
+
 
 
 function App() {
@@ -13,12 +15,13 @@ function App() {
 
   return (
     <>
-      <WelcomeMessage />
-      <Header />
-      <MainContent />
-      <Footer /> 
-
+    
       <div>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer /> 
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
