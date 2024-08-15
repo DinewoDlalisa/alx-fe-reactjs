7309b4978bf62fd0cb9d78bfe8b4ba366dbf9dee
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-15 21:29:25 +0200

    Duplicate react component and add css styling to them

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent () {
+    return (
+        <main style={{backgroundColor: 'pink', padding: '15px', textAlign: 'justify'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
