81239952ca94ea2ef141ddd7a77eeaa0a35377db
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-15 21:00:26 +0200

    Apply css to react components

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer () {
     return(
-        <footer>
+        <footer style={{backgroundColor: 'peachpuff', color: 'white', fontWeight:'lighter'}}>
             <p>© 2023 City Lovers</p>
         </footer>
     );
--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function MainContent () {
     return (
-        <main>
+        <main style={{backgroundColor: 'pink', padding: '15px', textAlign: 'justify'}}>
             <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
