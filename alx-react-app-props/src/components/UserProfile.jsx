7b994fa011c979ec3e7080aae68043548b7b0781
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-18 17:12:15 +0200

    Update Userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-const UserProfile = (props) => {
+import React, {useContext} from 'react';
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+    const {name, age, bio } = useContext(UserContext);
+
     return (
       <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <h2>{name}</h2>
+        <p>Age: {age}</p>
+        <p>Bio: {bio}</p>
       </div>
     );
   };
 
-  export default UserProfile;
+export default UserProfile;
