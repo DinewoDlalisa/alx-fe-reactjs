b3ff4227ca8355c38957c3d6cb1b428090cea4ba
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-21 22:51:44 +0200

    Update EditRecipeForm

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -7,9 +7,11 @@ const EditRecipeForm = ({recipe}) => {
     const [description, setDescription] = useState(recipe.description);
     const updateRecipe = useRecipeStore(state =>state.updateRecipe);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        updateRecipe({...recipe, title, description});
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const updateRecipe = {...recipe, title, description,};
+
+        updateRecipe(updateRecipe);
     };
 
     return (
