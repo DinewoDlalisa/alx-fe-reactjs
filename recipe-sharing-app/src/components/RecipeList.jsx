da183b21d1438238d1ae2b92286b4879e9884b49
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-24 00:10:12 +0200

    Implement advaced Search and filter

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,30 @@
 // RecipeList component
+import React,{useEffect} from 'react';
 import { useRecipeStore } from './recipeStore';
+import SearchBar from './SearchBar';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
+  const filterRecipes = useRecipeStore(state => state.filterRecipes);
+
+  useEffect(()=> {
+    filterRecipes();
+  },[filterRecipes]);
 
   return (
     <div>
-      {recipes.map(recipe => (
+      <SearchBar />
+    <div>
+      {recipes.length > 0 ? (recipes.map(recipe => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <p>{recipe.description}</p>
         </div>
-      ))}
+       ))
+      ):(
+       <p>No recipe</p>
+      )}
+      </div>
     </div>
   );
 };
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,9 +6,14 @@ const useRecipeStore = create(set => ({
   deleteRecipe: (recipeId) => set(state => ({recipes: state.recipes.filter(recipe => recipe.id != recipeId)})),
   updateRecipe: (updateRecipe) => set(state=> ({recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe: recipe)})),
   setRecipes: (recipes) => set({ recipes }),
+  searchTerm: '',
+  setSearchTerm: (term) => set(state=> {const newTerm = term.toLowerCase()
+    return {
+      searchTerm: newTerm,
+      filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowercase().includes(state.searchTerm.toLowerCase()))
+    }
+  })
+
 }));
-deleteRecipe: (recipeId) => set(state => ({
-  recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
-}))
 
 export {useRecipeStore};
