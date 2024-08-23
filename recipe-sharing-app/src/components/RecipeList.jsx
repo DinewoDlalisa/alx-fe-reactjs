268cb6011aeb6378ebe1b8cad5dbd4475d87752f
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-24 00:13:14 +0200

    import link hook from reactRouterDom

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,6 +2,7 @@
 import React,{useEffect} from 'react';
 import { useRecipeStore } from './recipeStore';
 import SearchBar from './SearchBar';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
