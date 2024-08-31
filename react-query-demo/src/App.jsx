9ba852bd939c5f6d3c043bf7d2d5e8cfd8e77e5d
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-08-31 14:54:04 +0200

    Update Query setup

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PostsComponent from './components/PostsComponent';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +32,15 @@ function App() {
       </p>
     </>
   )
-}
+  const queryClient = new QueryClient();
+
+  function App() {
+  return (
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
+   );
+  }
+};
 
 export default App
