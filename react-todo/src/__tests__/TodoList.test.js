10487aa0820c0e7a75f0dd39bc489456154ad83d
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-01 13:08:38 +0200

    Adjust line endings

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent} from '@testing-library/react';
-import '@testing-library/jest-dom'
-import TodoList from '../components/TodoList'
+import '@testing-library/jest-dom';
+import TodoList from '../components/TodoList';
 
 
 test ('renders TodoList components', () =>{
