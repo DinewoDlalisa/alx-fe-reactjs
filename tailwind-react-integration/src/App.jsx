5d7325060f1b447826d914b4f017fa28927fee1e
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-05 15:22:03 +0200

    Import UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import userProfile from './components/UserProfile'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <UserProfile/>
+     </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
