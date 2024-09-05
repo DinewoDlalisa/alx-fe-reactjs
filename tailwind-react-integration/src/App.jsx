8fac57aa58c4675b3cae4afe0e23212dcbb0db2a
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-05 17:07:36 +0200

    adjust css to be compatible to different screens

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import userProfile from './components/UserProfile'
 import UserProfile from './components/UserProfile'
 
 function App() {
