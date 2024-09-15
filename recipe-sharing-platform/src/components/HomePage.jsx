3bb68554cdb4bfc296da4c03c6397b55590db484
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-15 07:01:56 +0200

    Update Hompage grid prop

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     return(
      <div className="container mx-auto p-4">
          <h1 className="text-3xl font-bold mb-6">Recipe Sharing Platform</h1>
-         <div className="grid grid-cols-1 sm:grid-cols md:grid-cols-3 lg:grid-cols-4 gap-6">
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
              {recipes.map((recipe)=> (
                  <div key={recipe.id} className="bg-white rounded-lg shadow-lg overflow-hidden hover:shadow-x1 transition-shadow">
                      <img src= {recipe.image} alt={recipe.title} className="w-full h-48 object-cover"/>
@@ -29,7 +29,6 @@ const HomePage = () => {
                 
         </div>
     );
-
 
 
 
