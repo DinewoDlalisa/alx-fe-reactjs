96f3bdff93997d67ef3c133e60636270438c245c
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-23 22:42:38 +0200

    Include API URL

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,20 +4,29 @@ const API_URL = 'https://api.github.com';
 const API_KEY = process.env.REACT_APP_GITHUB_KEY;
 
 
-export const fetchUserData = async (username) => {
-    const response = await axios.get('${API_URL}/search/users', {
+export const searchGitHubUsers = async ({ username, location, minRepos }) => {
+   
+    let query = '';
+
+    if (username) {
+        query += `q=${username}`;
+    }
+
+    if (location) {
+        query += `+location:${location}`;
+    }
+
+    if (minRepos) {
+        query += `+repos:>=${minRepos}`;
+    }
+
+ 
+    const url = `${API_URL}/search/users?${query}`;
+
+  
+    return axios.get(url, {
         headers: {
-            Authorization: 'token ${API_KEY}'
-
+            Authorization: `token ${API_KEY}`
         }
-    });
-    return response.data;
-};
-
-export const searchGitHubUsers = ({ username, location, minRepos }) => {
-    const query = `${username} ${location ? `location:${location}` : ''} ${minRepos ? `repos:>${minRepos}` : ''}`.trim();
-    return axios.get('${API_URL}/search/users', {
-        params: { q: query },
-        headers: {Authorization: 'token ${API_KEY}'}
     });
 };
