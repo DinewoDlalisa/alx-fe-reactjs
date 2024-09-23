040218c084cc9f08f02b3bf09243840e08a62bc0
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-23 21:42:31 +0200

    Update Search file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,12 +1,25 @@
 import React, {useState} from "react";
 
-const Search = ({ onSearch, users, loading, error }) => {
+const Search = ({ onSearch }) => {
     const [username, setUsername] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [users, setUsers] = useState([]);
 
     const handleSubmit = (element) => {
         element.preventDefault();
         if (username.trim()) {
-            onSearch(username);
+           setLoading(true);
+           setError('');
+           setUsers([]);
+           try {
+            const fetchedUsers = await onSearch(username);
+            setUsers(fetchedUsers);
+           }catch (err) {
+            setError("User not found")
+           } finally {
+            setLoading(false);
+           }
         }
     };
 
@@ -16,10 +29,25 @@ const Search = ({ onSearch, users, loading, error }) => {
                 <input type="text" value={username} onChange={(element)=> setUsername(element.target.value)} placeholder="Enter username"/>
                 <button type="submit">Search</button>
             </form>
+           
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {users.length > 0 && (
+                <div>
+                    {users.map(user => (
+                        <div key={user.id}>
+                            <img src={user.avatar_url} alt={user.login} width="100" />
+                            <h2>{user.login}</h2>
+                            <a href={user.html_url} target="_blank" rel="noopener noreferrer">
+                                View Profile
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
-
-
+                
 export default Search;
 
