77853aad09f5c281016799f5219d8c85263ac012
Dinewo Philisiwe Dlalisa <DlalisaD@ukzn.ac.za>
2024-09-23 22:52:06 +0200

    add full URL in githubService fil

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const API_URL = 'https://api.github.com';
+const API_URL = 'https://api.github.com/search/users?q={query}';
 const API_KEY = process.env.REACT_APP_GITHUB_KEY;
 
-
 export const searchGitHubUsers = async ({ username, location, minRepos }) => {
-   
     let query = '';
 
     if (username) {
@@ -20,12 +18,10 @@ export const searchGitHubUsers = async ({ username, location, minRepos }) => {
         query += `+repos:>=${minRepos}`;
     }
 
- 
-    const url = `${API_URL}/search/users?${query}`;
+    const url = `https://api.github.com/search/users?q=${query}`;
 
-    console.log('Fetching from: ${url}');
+    console.log(`Fetching from: ${url}`);
 
-  
     return axios.get(url, {
         headers: {
             Authorization: `token ${API_KEY}`
